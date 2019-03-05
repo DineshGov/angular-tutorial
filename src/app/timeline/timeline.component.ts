b8602dccd2b66776ee20b44496695b18bda3bff0
DineshGov <gov.dinesh@gmail.com>
2019-03-05 14:56:58 +0100

    like/dislike tweet added

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -13,13 +13,15 @@ export class TimelineComponent implements OnInit {
             created_at: 'Wed Apr 05 12:30:12 +0000 2017',
             id: 1,
             text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
-            user: 'James Braddock'
+            user: 'James Braddock',
+            nbrLike: 0,
         },
         {
             created_at: 'Thu Apr 06 15:24:15 +0000 2017',
             id: 2,
             text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
-            user: 'Bob kelso'
+            user: 'Bob kelso',
+            nbrLike: 0,
         },
     ];
 
@@ -33,10 +35,37 @@ export class TimelineComponent implements OnInit {
         newTweet.id = this.tweets.length + 1;
         newTweet.text = tweetText;
         newTweet.user = 'Dinesh';
+        newTweet.nbrLike = 0;
 
         this.tweets.push(newTweet);
 
         console.log(this.tweets);
+    }
+
+    onLike(tweet: object , like: HTMLInputElement, unlike: HTMLInputElement) {
+        if(tweet.nbrLike <= 0) {
+            tweet.nbrLike = 1;
+
+            like.className = 'fas fa-heart';
+
+            like.style.color = 'red';
+            unlike.style.color = 'black';
+        }
+    }
+
+    onDislike(tweet: object , like: HTMLInputElement, unlike: HTMLInputElement) {
+        if(tweet.nbrLike >= 0){
+            tweet.nbrLike = -1;
+
+            like.className = 'far fa-heart';
+
+            unlike.style.color = 'red';
+            like.style.color = 'black';
+        }
+    }
+
+    onHover(heart: HTMLInputElement) {
+        heart.style.cursor = 'pointer';
     }
 
     ngOnInit() {
