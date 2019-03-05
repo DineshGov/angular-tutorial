80b9411a05ea2467144c6ab52f66878d5a841970
DineshGov <gov.dinesh@gmail.com>
2019-03-05 11:05:00 +0100

    timeline component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
