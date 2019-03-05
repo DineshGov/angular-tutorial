38d8547e1390943a3b7245a7b9f50b7992460f00
DineshGov <gov.dinesh@gmail.com>
2019-03-05 15:40:09 +0100

    Forms module installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { TimelineComponent } from './timeline/timeline.component';
-import { TimeAgoPipe } from 'time-ago-pipe';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { TimeAgoPipe } from 'time-ago-pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
